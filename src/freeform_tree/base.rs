d0c737a1e28693d2b132aed0d0c2739f54a533e7
Kotauskas <v.toncharov@gmail.com>
2021-03-12 23:28:11 +0300

    Added Default impl to FreeformTree
    Was missing due to an oversight, saw it during the refactor.

--- src/freeform_tree/base.rs
+++ src/freeform_tree/base.rs
@@ -184,3 +184,14 @@ where
         self.storage.is_dense()
     }
 }
+
+impl<B, L, K, S> Default for FreeformTree<B, L, K, S>
+where
+    L: Default,
+    S: Storage<Element = Node<B, L, K>, Key = K>,
+    K: Clone + Debug + Eq,
+{
+    fn default() -> Self {
+        Self::new(L::default())
+    }
+}
